ae74ca1512c1eecd731f2f939dd98877b654dc34
Rossenzii <jangmj80@naver.com>
2025-10-29 20:35:05 +0900

    [fix] project to all

--- src/main/java/BITAmin/BE/global/config/SecurityConfig.java
+++ src/main/java/BITAmin/BE/global/config/SecurityConfig.java
@@ -26,6 +26,7 @@ public class SecurityConfig {
                 .sessionManagement(session -> session.sessionCreationPolicy(SessionCreationPolicy.STATELESS))
                 .authorizeHttpRequests(auth -> auth
                         .requestMatchers("/api/auth/register","/api/auth/login", "/api/members/all",
+                        "/api/project/**",
                         "/swagger-ui/**",
                         "/swagger-resources/**",
                         "/v3/api-docs/**",
